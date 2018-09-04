f0c8b44a5719fb7fffc63d25547e0cea35f2f0de
Juha Jeronen <juha.jeronen@jyu.fi>
2018-09-04 13:26:24 +0300

    make phase 2 macro to generate the with-curry forms

--- spicy/sauce.rkt
+++ spicy/sauce.rkt
@@ -12,33 +12,31 @@ require (only-in spicy/expander [#%app spicy-app]
 
 provide with-curry splicing-with-curry
 
-define-syntax-parser with-curry
-  [_ body ...+]
-    with-syntax ([#%app (datum->syntax this-syntax '#%app)]
-                 [curry (datum->syntax this-syntax 'curry)]
-                 [compose (datum->syntax this-syntax 'compose)])
-      syntax
-        let-syntax ([#%app (make-rename-transformer #'spicy-app)]
-                    [curry (make-rename-transformer #'spicy-curry)]
-                    [compose (make-rename-transformer #'spicy-compose)])
-          body
-          ...
-  [_]
-    raise-syntax-error 'with-curry "needs at least one body"
+require (for-meta 2 racket/base)
+require (for-syntax syntax/parse/define)
+begin-for-syntax
+  define-syntax-parser make-with-curry-form
+    [_ name the-let-stx]
+      with-syntax ([ooo (quote-syntax ...)]
+                   [ooo+ (quote-syntax ...+)])
+        syntax
+          let ([name-sym (syntax->datum #'name)])
+            syntax-parser
+              [_ body ooo+]
+                with-syntax ([#%app (datum->syntax this-syntax '#%app)]
+                             [curry (datum->syntax this-syntax 'curry)]
+                             [compose (datum->syntax this-syntax 'compose)])
+                  syntax
+                    the-let-stx ([#%app (make-rename-transformer #'spicy-app)]
+                                 [curry (make-rename-transformer #'spicy-curry)]
+                                 [compose (make-rename-transformer #'spicy-compose)])
+                      body
+                      ooo
+              [_]
+                raise-syntax-error name-sym "expected at least one body"
 
-define-syntax-parser splicing-with-curry
-  [_ body ...+]
-    with-syntax ([#%app (datum->syntax this-syntax '#%app)]
-                 [curry (datum->syntax this-syntax 'curry)]
-                 [compose (datum->syntax this-syntax 'compose)])
-      syntax
-        splicing-let-syntax ([#%app (make-rename-transformer #'spicy-app)]
-                             [curry (make-rename-transformer #'spicy-curry)]
-                             [compose (make-rename-transformer #'spicy-compose)])
-          body
-          ...
-  [_]
-    raise-syntax-error 'splicing-with-curry "needs at least one body"
+define-syntax with-curry (make-with-curry-form with-curry let-syntax)
+define-syntax splicing-with-curry (make-with-curry-form splicing-with-curry splicing-let-syntax)
 
 module+ main
   ;; block with local spicification
